7c9c655b653ac3146dd3c626977a0155bf9539b0
auco1120 <auco1120@outlook.com>
2025-03-31 21:01:44 -0600

    Add MyComponent and its tests
    This commit introduces the `MyComponent` composable, which includes a TextField for user input and displays a greeting with the entered name.
    
    Additionally, it adds a suite of tests for `MyComponent` to verify its functionality:
    
    - **Finder Tests:** Tests ensure that elements like text, tags, and content descriptions can be correctly located within the composable.
    - **Action Tests:** Tests simulate user interactions such as clicks, double clicks, long clicks, swipes, scrolling, and text input/replacement.
    - **Assertion Tests:** Tests validate the state and properties of the composable, including visibility, enabled/disabled state, selection, focus, text content, and content descriptions.
    - **Content Verification Tests:** Tests specifically check if the initial content of the TextField is "ElGG" and if the greeting text updates correctly when a new name is entered.

--- app/src/androidTest/java/com/cursokotlin/composetesting/components/MyComponentTest.kt
+++ app/src/androidTest/java/com/cursokotlin/composetesting/components/MyComponentTest.kt
@@ -1,18 +1,114 @@
 package com.cursokotlin.composetesting.components
 
+import androidx.compose.ui.test.assertContentDescriptionContains
+import androidx.compose.ui.test.assertContentDescriptionEquals
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsEnabled
+import androidx.compose.ui.test.assertIsFocused
+import androidx.compose.ui.test.assertIsNotDisplayed
+import androidx.compose.ui.test.assertIsNotEnabled
+import androidx.compose.ui.test.assertIsNotFocused
+import androidx.compose.ui.test.assertIsNotSelected
+import androidx.compose.ui.test.assertIsOff
+import androidx.compose.ui.test.assertIsOn
+import androidx.compose.ui.test.assertIsSelected
+import androidx.compose.ui.test.assertTextContains
+import androidx.compose.ui.test.assertTextEquals
+import androidx.compose.ui.test.doubleClick
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.longClick
+import androidx.compose.ui.test.onAllNodesWithContentDescription
+import androidx.compose.ui.test.onAllNodesWithTag
+import androidx.compose.ui.test.onAllNodesWithText
+import androidx.compose.ui.test.onFirst
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performImeAction
+import androidx.compose.ui.test.performScrollTo
+import androidx.compose.ui.test.performTextClearance
+import androidx.compose.ui.test.performTextInput
+import androidx.compose.ui.test.performTextReplacement
+import androidx.compose.ui.test.performTouchInput
+import androidx.compose.ui.test.swipeDown
+import androidx.compose.ui.test.swipeLeft
+import androidx.compose.ui.test.swipeRight
+import androidx.compose.ui.test.swipeUp
 import org.junit.Rule
 import org.junit.Test
 
 class MyComponentTest {
 
     @get:Rule
-    val composeRule = createComposeRule()
+    val composeTestRule = createComposeRule()
 
     @Test
     fun myFirstTest() {
-        composeRule.setContent {
+        composeTestRule.setContent {
             MyComponent()
         }
+
+        //FINDER
+        composeTestRule.onNodeWithText("ElGG", ignoreCase = true)
+        composeTestRule.onNodeWithTag("component1")
+        composeTestRule.onNodeWithContentDescription("add")
+
+        composeTestRule.onAllNodesWithText(":")
+        composeTestRule.onAllNodesWithTag("component1")
+        composeTestRule.onAllNodesWithContentDescription("visualIcon")
+
+        //ACTIONS
+        composeTestRule.onNodeWithText("ElGG", ignoreCase = true).performClick()
+        composeTestRule.onAllNodesWithText("e").onFirst().performClick()
+        composeTestRule.onNodeWithText("ElGG").performTouchInput {
+            doubleClick()
+            longClick()
+            swipeDown()
+            swipeUp()
+            swipeLeft()
+            swipeRight()
+        }
+        composeTestRule.onNodeWithText("ElGG").performScrollTo()
+        composeTestRule.onNodeWithText("ElGG").performImeAction()
+        composeTestRule.onNodeWithText("ElGG").performTextClearance()
+        composeTestRule.onNodeWithText("ElGG").performTextInput("Se la come")
+        composeTestRule.onNodeWithText("ElGG").performTextReplacement("Selacome")
+
+        //ASSERTIONS
+        composeTestRule.onNodeWithText("ElGG").assertExists()
+        composeTestRule.onNodeWithText("ElGG").assertDoesNotExist()
+        composeTestRule.onNodeWithText("ElGG").assertContentDescriptionContains("")
+        composeTestRule.onNodeWithText("ElGG").assertContentDescriptionEquals("")
+        composeTestRule.onNodeWithText("ElGG").assertIsDisplayed()
+        composeTestRule.onNodeWithText("ElGG").assertIsNotDisplayed()
+        composeTestRule.onNodeWithText("ElGG").assertIsEnabled()
+        composeTestRule.onNodeWithText("ElGG").assertIsNotEnabled()
+        composeTestRule.onNodeWithText("ElGG").assertIsSelected()
+        composeTestRule.onNodeWithText("ElGG").assertIsNotSelected()
+        composeTestRule.onNodeWithText("ElGG").assertIsNotFocused()
+        composeTestRule.onNodeWithText("ElGG").assertIsFocused()
+        composeTestRule.onNodeWithText("ElGG").assertIsOn()
+        composeTestRule.onNodeWithText("ElGG").assertIsOff()
+        composeTestRule.onNodeWithText("ElGG").assertTextContains("")
+    }
+
+    @Test
+    fun whenComponentStart_thenVerifyContentIsElGG() {
+        composeTestRule.setContent {
+            MyComponent()
+        }
+        composeTestRule.onNodeWithText("ElGG", ignoreCase = true).assertExists()
+        composeTestRule.onNodeWithTag("textFieldName").assertTextContains("ElGG", ignoreCase = true)
+    }
+
+    @Test
+    fun whenNameIsAdded_thenVerifyTextContainsGreeting() {
+        composeTestRule.setContent {
+            MyComponent()
+        }
+
+        composeTestRule.onNodeWithTag("textFieldName").performTextReplacement("ElGG")
+        composeTestRule.onNodeWithTag("textGreeting").assertTextEquals("Te llamas ElGG")
     }
 }
--- app/src/main/java/com/cursokotlin/composetesting/components/MyComponent.kt
+++ app/src/main/java/com/cursokotlin/composetesting/components/MyComponent.kt
@@ -1,9 +1,29 @@
 package com.cursokotlin.composetesting.components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 
+@Preview
 @Composable
 fun MyComponent() {
+    var name by rememberSaveable { mutableStateOf("ElGG") }
 
+    Column(Modifier.fillMaxSize().padding(24.dp)) {
+        TextField(value = name, onValueChange = { name = it }, Modifier.testTag("textFieldName").fillMaxWidth())
+        Text("Te llamas $name", Modifier.testTag("textGreeting"))
+    }
 }
